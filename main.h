76d592b944040ccd71fb742df46475cfefc33f0b
Salomon700 <ntwalisolomon9@gmail.com>
2024-01-28 02:29:12 +0200

    fixed namening and mupliple definitions

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
-#include <stdarg.h>
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 int charPrint(int count, va_list args);
